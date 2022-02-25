352df64eec1ba9e9b16e506401c2979db2b41104
sanghyun-k <sanghyun.k@navercorp.com>
2022-02-25 14:40:41 +0900

    decentralized-short-sale revise code

--- src/main/java/com/github/espress91/decentralizedShortSale/service/EthereumService.java
+++ src/main/java/com/github/espress91/decentralizedShortSale/service/EthereumService.java
@@ -1,7 +1,6 @@
 package com.github.espress91.decentralizedShortSale.service;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.FunctionReturnDecoder;
@@ -22,36 +21,30 @@ import java.util.concurrent.ExecutionException;
 @Component
 public class EthereumService {
 
-    @Value("${ethereum.contract.address}")
     private String contract;
-
-    @Value("${ethereum.owner.address}")
     private String from;
-
-    @Value("${ethereum.owner.passwd}")
     private String pwd;
 
     private Admin web3j;
 
-    public EthereumService()
-    {
+    public EthereumService(@Value("${ethereum.contract.address}") String contract,
+                           @Value("${ethereum.owner.address}") String from,
+                           @Value("${ethereum.owner.passwd}") String pwd) {
+        this.contract = contract;
+        this.from = from;
+        this.pwd = pwd;
         web3j = Admin.build(new HttpService()); // default server : http://localhost:8545
     }
 
     public Object ethCall(Function function) throws IOException {
-        // 1. Account Lock 해제
         PersonalUnlockAccount personalUnlockAccount = web3j.personalUnlockAccount(from, pwd).send();
 
-        if (personalUnlockAccount.accountUnlocked()) { // unlock 일때
-
-            //2. transaction 제작
+        if (personalUnlockAccount.accountUnlocked()) {
             Transaction transaction = Transaction.createEthCallTransaction(from, contract,
-                                                                           FunctionEncoder.encode(function));
+                    FunctionEncoder.encode(function));
 
-            //3. ethereum 호출후 결과 가져오기
             EthCall ethCall = web3j.ethCall(transaction, DefaultBlockParameterName.LATEST).send();
 
-            //4. 결과값 decode
             List<Type> decode = FunctionReturnDecoder.decode(ethCall.getResult(),
                                                              function.getOutputParameters());
 
@@ -60,68 +53,41 @@ public class EthereumService {
             System.out.println("getType = " + decode.get(0).getTypeAsString());
 
             return decode.get(0).getValue();
-        }
-        else
-        {
+        } else {
             throw new PersonalLockException("check ethereum personal Lock");
         }
     }
 
-    public String ethSendTransaction(Function function)
-            throws IOException, InterruptedException, ExecutionException {
-
-        // 1. Account Lock 해제
+    public String ethSendTransaction(Function function) throws IOException, InterruptedException, ExecutionException {
         PersonalUnlockAccount personalUnlockAccount = web3j.personalUnlockAccount(from, pwd).send();
 
-        if (personalUnlockAccount.accountUnlocked()) { // unlock 일때
-
-            //2. account에 대한 nonce값 가져오기.
-            EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(
-                    from, DefaultBlockParameterName.LATEST).sendAsync().get();
+        if (personalUnlockAccount.accountUnlocked()) {
+            EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(from, DefaultBlockParameterName.LATEST).sendAsync().get();
 
             BigInteger nonce = ethGetTransactionCount.getTransactionCount();
-
-            //3. Transaction값 제작
-            Transaction transaction = Transaction.createFunctionCallTransaction(from, nonce,
-                                                                                Transaction.DEFAULT_GAS,
-                                                                                null, contract,
-                                                                                FunctionEncoder.encode(function));
-
-            // 4. ethereum Call &
+            Transaction transaction = Transaction.createFunctionCallTransaction(from, nonce, Transaction.DEFAULT_GAS, null, contract, FunctionEncoder.encode(function));
             EthSendTransaction ethSendTransaction = web3j.ethSendTransaction(transaction).send();
-
-            // transaction에 대한 transaction Hash값 얻기.
             String transactionHash = ethSendTransaction.getTransactionHash();
 
-            // ledger에 쓰여지기 까지 기다리기.
             Thread.sleep(5000);
 
             return transactionHash;
-        }
-        else {
+        } else {
             throw new PersonalLockException("check ethereum personal Lock");
         }
     }
 
     public TransactionReceipt getReceipt(String transactionHash) throws IOException {
-
         EthGetTransactionReceipt transactionReceipt = web3j.ethGetTransactionReceipt(transactionHash).send();
-
-        if(transactionReceipt.getTransactionReceipt().isPresent())
-        {
-            System.out.println("transactionReceipt.getResult().getContractAddress() = " +
-                               transactionReceipt.getResult());
-        }
-        else
-        {
+        if(transactionReceipt.getTransactionReceipt().isPresent()) {
+            System.out.println("transactionReceipt.getResult().getContractAddress() = " + transactionReceipt.getResult());
+        } else {
             System.out.println("transaction complete not yet");
         }
-
         return transactionReceipt.getResult();
     }
 
-    private class PersonalLockException extends RuntimeException
-    {
+    private static class PersonalLockException extends RuntimeException {
         public PersonalLockException(String msg)
         {
             super(msg);
